2ed5a1ac2a974d761f132c0d73e26b9464743800
Rustam <mirzamorf@yandex.ru>
2020-07-05 01:00:41 +0300

    updated files

---
+++ src/data.js
@@ -0,0 +1,17 @@
+export const data = [
+  {
+    description: 'Метка',
+    coords: [59.93493436755984,30.326365330109287],
+    mark: 'islands#darkOrangeDotIcon'
+  },
+  {
+    description: 'Метка',
+    coords: [60.00739376851526,30.331950021405486],
+    mark: 'islands#pinkDotIcon'
+  },
+  {
+    description: 'Метка',
+    coords: [59.94423671023645,30.41837582815614],
+    mark: 'islands#darkGreenDotIcon'
+  }
+]
--- src/index.js
+++ src/index.js
@@ -1,11 +1,36 @@
+import { data } from './data'
 import './css/main.scss'
 
-class Person {
-  constructor(name, age) {
-    this.name = name
-    this.age = age
-  }
-}
+ymaps.ready(() => {
 
-const person = new Person('Rustam', 24)
-console.log(person)
+  const mapPiter = new ymaps.Map("map", {
+    center: [59.91807704072416,30.304899499999895],
+    zoom: 10,
+    controls: []
+  },{
+    suppressMapOpenBlock: true
+    // restrictMapArea: [[59.45491523077794,29.634223984005246],[60.131770983132974,31.254707382442746]]
+  });
+
+
+  data.forEach(({ coords, mark, description}, index) => {
+    mapPiter.geoObjects.add(
+      new ymaps.Placemark(coords, {
+        balloonContent: `${description} ${index + 1}`
+      }, {
+        preset: mark
+      })
+    )
+  })
+
+
+  document.querySelector('.change-map').onclick = () => {
+    // mapPiter.setCenter([57.767265, 40.925358])
+    // mapPiter.setType('yandex#satellite')
+    mapPiter.panTo([55.65280597843513,37.634816459742524],{
+      // duration: 1300
+      flying: true
+    })
+
+  }
+})
